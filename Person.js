d761ca9d1e1533ba2e812ce0e21f890269343dca
Dima Matroskin <dima.matroskin062@gmail.com>
2021-02-04 00:56:34 +0200

    Person and PersonLog implemented

--- Person.js
+++ Person.js
@@ -1,28 +1,59 @@
 class Person {
+  //TODO: Make constant
   static GENDER = {
     NOT_DEFINED: 0,
     MAN: 1,
     WOMAN: 2,
   };
-  name = 'NoName';
-  gender = Person.GENDER.NOT_DEFINED;
 
-  constructor(name, gender) {
+  #gender = Person.GENDER.NOT_DEFINED;
+
+  constructor(name = 'NoName', gender) {
+    this.name = name;
+    this.#gender = gender;
+  }
+
+  get gender() {
+    return this.#gender;
+  }
+
+  set gender(gender) {
     if (!Object.values(Person.GENDER).includes(gender)) {
       throw new PersonGenderError();
     }
-    this.name = name;
-    this.gender = gender;
+    this.#gender = gender;
   }
 }
 
 class PersonGenderError extends Error {
-  #log = [];
-
   constructor(message) {
     super(message);
-    this.name = 'PersonGenderError';
   }
 }
 
-class PersonLog extends Person {}
+class PersonLog extends Person {
+  #log = [];
+
+  constructor(name, gender) {
+    super(name, gender);
+    const proxy = new Proxy(this, {
+      get(target, prop) {
+        return Reflect.get(target, prop);
+      },
+      set(target, prop, value) {
+        if (target[prop] !== value) {
+          if (target[prop]) {
+            target.#log.push(`${prop}: ${target[prop]} -> ${value}`);
+          }
+          return Reflect.set(target, prop, value);
+        }
+        return true;
+      },
+    });
+    return Object.seal(proxy);
+  }
+
+  get log() {
+    return this.#log.length ? this.#log : 'no logs yet';
+  }
+}
