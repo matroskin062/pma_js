8d3393a54a4852bbaa1ad108df71067381792270
Dima Matroskin <dima.matroskin062@gmail.com>
2021-02-04 14:17:37 +0200

    fixes

--- Person.js
+++ Person.js
@@ -1,5 +1,4 @@
 class Person {
-  //TODO: Make constant
   static GENDER = {
     NOT_DEFINED: 0,
     MAN: 1,
@@ -8,7 +7,7 @@ class Person {
 
   #gender = Person.GENDER.NOT_DEFINED;
 
-  constructor(name = 'NoName', gender) {
+  constructor(name = 'NoName', gender = Person.GENDER.NOT_DEFINED) {
     this.name = name;
     this.#gender = gender;
   }
@@ -26,10 +25,21 @@ class Person {
 }
 
 class PersonGenderError extends Error {
-  constructor(message) {
+  constructor(message = 'Invalid gender value') {
     super(message);
   }
 }
+
+console.log('====================================');
+console.log('Person');
+console.log('====================================');
+
+const person = new Person();
+person.gender = 1;
+//works correct
+console.log(person.gender);
+//will throw exception
+// person.gender = 10;
 
 class PersonLog extends Person {
   #log = [];
@@ -42,12 +52,11 @@ class PersonLog extends Person {
       },
       set(target, prop, value) {
         if (target[prop] !== value) {
-          if (target[prop]) {
+          if (target[prop] && prop !== 'log') {
             target.#log.push(`${prop}: ${target[prop]} -> ${value}`);
           }
           return Reflect.set(target, prop, value);
         }
-        return true;
       },
     });
     return Object.seal(proxy);
@@ -57,3 +66,26 @@ class PersonLog extends Person {
     return this.#log.length ? this.#log : 'no logs yet';
   }
 }
+
+console.log('====================================');
+console.log('Person with Logger');
+console.log('====================================');
+
+const personLog = new PersonLog();
+
+personLog.name = 'foo';
+personLog.name = 'bar';
+personLog.name = 'baz';
+
+personLog.gender = 0;
+personLog.gender = 1;
+personLog.gender = 2;
+//will throw exception
+// personLog.gender = 10;
+
+//cannot add new properties
+personLog.newProp = 'new';
+//will print undefined
+console.log(personLog.newProp);
+
+console.log(personLog.log);
